0e90c78979a9f8be114df2dff8cd7a3db55faabf
Brett Vickers <brett@beevik.com>
2015-03-11 13:59:57 -0700

    Add initial login logic.

--- game.go
+++ game.go
@@ -1,16 +1,22 @@
 package unimud
 
+import "log"
+
 // A Game is an instance of a uniMUD game.
 type Game struct {
 }
 
+// NewGame creates a new unimud game instance.
 func NewGame() *Game {
 	return new(Game)
 }
 
+// ListenConsole listens for player input on standard input
+// and outputs text on standard output.
 func (g *Game) ListenConsole() {
 	for {
 		p := newPlayer(g)
 		p.run()
+		log.Println("Player exited")
 	}
 }
--- player.go
+++ player.go
@@ -2,8 +2,11 @@ package unimud
 
 import (
 	"bufio"
+	"encoding/gob"
+	"errors"
 	"fmt"
 	"os"
+	"path"
 )
 
 // A player represents a user playing the unimud Game instance.
@@ -22,15 +25,104 @@ func newPlayer(g *Game) *player {
 	}
 }
 
-func (p *player) run() {
-	for {
-		fmt.Fprint(p.output, "login: ")
-		p.output.Flush()
+type playerState func(p *player) playerState
 
-		if p.input.Scan() {
-			login := p.input.Text()
-			fmt.Fprintln(p.output, "Hi", login)
-			p.output.Flush()
-		}
+func (p *player) run() {
+	state := (*player).stateLogin
+	for state != nil {
+		state = state(p)
 	}
+}
+
+func (p *player) Print(args ...interface{}) {
+	fmt.Fprint(p.output, args...)
+	p.output.Flush()
+}
+
+func (p *player) Println(args ...interface{}) {
+	fmt.Fprintln(p.output, args...)
+	p.output.Flush()
+}
+
+func (p *player) GetLine() (line string, err error) {
+	p.output.Flush()
+	if p.input.Scan() {
+		line := p.input.Text()
+		return line, nil
+	}
+	return "", errors.New("player: disconnected")
+}
+
+func (p *player) stateLogin() playerState {
+	p.Print("login: ")
+	login, err := p.GetLine()
+	if err != nil {
+		return nil
+	}
+
+	switch {
+	case len(login) < 4:
+		p.Println("login id is too short.")
+		return (*player).stateLogin
+	case len(login) > 32:
+		p.Println("login id is too long.")
+		return (*player).stateLogin
+	case !validateLogin(login):
+		p.Println("login id contains invalid characters.")
+		return (*player).stateLogin
+	}
+
+	p.login = login
+	if err := p.load(); err != nil {
+		// TODO: go to create new player state
+		return nil
+	}
+
+	p.Print("password: ")
+	pw, err := p.GetLine()
+	if err != nil {
+		return nil
+	}
+
+	_ = pw
+
+	// TODO: Check the password
+	// TODO: If valid, return the playing-game state func
+	return nil
+}
+
+func validateLogin(login string) bool {
+	for _, c := range login {
+		switch {
+		case c >= 'a' && c <= 'z':
+			continue
+		case c >= 'A' && c <= 'Z':
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+func (p *player) load() error {
+	filename := path.Join("players", p.login+".dat")
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	dec := gob.NewDecoder(f)
+
+	var login string
+	if err := dec.Decode(&login); err != nil {
+		return err
+	}
+	if login != p.login {
+		return errors.New("player: login id mismatch")
+	}
+
+	// TODO: Decode the rest of the player's data
+
+	return nil
 }
